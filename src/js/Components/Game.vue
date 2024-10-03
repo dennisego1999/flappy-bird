62dbc2711b441d0d79aa560efda2e78d082bf5cd
Dennis Ego <dennis.ego@artcoresociety.com>
2024-10-03 08:13:30 +0200

    Update layout

--- src/js/Components/Game.vue
+++ src/js/Components/Game.vue
@@ -22,23 +22,23 @@ onBeforeUnmount(() => {
 
 <template>
 	<div class="relative flex h-[100dvh] w-screen items-center justify-center bg-green-400">
-		<Transition name="fade" mode="out-in">
-			<div
-				v-if="Game.isGameOver.value"
-				class="absolute inset-0 z-10 flex h-full w-full flex-col items-center justify-center gap-4 bg-black/[0.5]"
-			>
-				<span class="rounded-lg bg-white p-2 font-extrabold">Game over</span>
-				<span class="rounded-lg bg-gray-400 p-2 font-extrabold text-white">
-					Je had een score van {{ Game.score.value }}
-				</span>
-			</div>
-
-			<div v-else class="absolute right-2 top-2 z-50 rounded-lg bg-white p-2">
-				<span class="font-extrabold text-black">Score: {{ Game.score.value }}</span>
-			</div>
-		</Transition>
-
 		<div class="relative aspect-[390/844] w-full overflow-hidden rounded-lg shadow-2xl sm:w-[390px]">
+			<Transition name="fade" mode="out-in">
+				<div
+					v-if="Game.isGameOver.value"
+					class="absolute inset-0 z-10 flex h-full w-full flex-col items-center justify-center gap-4 bg-black/[0.5]"
+				>
+					<span class="rounded-lg bg-white p-2 font-extrabold">Game over</span>
+					<span class="rounded-lg bg-gray-400 p-2 font-extrabold text-white">
+						Je had een score van {{ Game.score.value }}
+					</span>
+				</div>
+
+				<div v-else class="absolute right-2 top-2 z-50 rounded-lg bg-white p-2">
+					<span class="font-extrabold text-black">Score: {{ Game.score.value }}</span>
+				</div>
+			</Transition>
+
 			<canvas id="game-canvas" class="h-full w-full" />
 		</div>
 	</div>
